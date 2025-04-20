390356a59c75dea7708f65822c25b6dc4ec08a99
Sebastián Jhoel <sebastian777campos@gmail.com>
2025-04-20 16:04:43 -0400

    hecho en grupo

---
+++ src/main/java/ALOJAMIENTO/ALOJAMIENTO/AlojamientoApplication.java
@@ -0,0 +1,13 @@
+package ALOJAMIENTO.ALOJAMIENTO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlojamientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlojamientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ALOJAMIENTO/ALOJAMIENTO/AlojamientoApplicationTests.java
@@ -0,0 +1,13 @@
+package ALOJAMIENTO.ALOJAMIENTO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlojamientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
